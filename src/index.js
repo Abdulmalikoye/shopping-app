51346db5178701e4277b13fb36476f6297e85dfd
Abdulmalikoye <106560197+Abdulmalikoye@users.noreply.github.com>
2024-12-23 09:44:18 +0100

    Create user endpoint and code clean up

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ const mongoose = require("mongoose");
 const app = express();
 const dotenv = require("dotenv");
 const port = 3000;
+const userRoutes = require("./routes/user");
+app.use(express.json());
+app.use("/user", userRoutes);
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log("Mongodb connected");
 });
+
 app.listen(port, () => {
   console.log("The server is running on port 3000");
 });
+module.exports = app;
